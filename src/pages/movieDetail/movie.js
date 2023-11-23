7514c78481fd6c2bb23ebf3ae494bfdd17b0be57
“RTSaitama” <RTSaitama@outlook.com>
2023-11-23 18:54:37 +0200

    carousel deleted from json

--- src/pages/movieDetail/movie.js
+++ src/pages/movieDetail/movie.js
@@ -16,7 +16,7 @@ const Movie = () => {
     // другий запит  на більш детальний опис
     const getData = () => {
         fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=4e44d9029b1270a757cddc766a1bcb63`)
-        .then(res => res.json())
+        .then(res => res.json())1
         .then(data => setMovie(data))
     }
 
