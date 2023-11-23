03af4b507f387df88571c37e003c034d91ac716f
“RTSaitama” <RTSaitama@outlook.com>
2023-11-23 20:20:21 +0200

    deleted defer

--- src/pages/movieDetail/movie.js
+++ src/pages/movieDetail/movie.js
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from "react"
  
 import { useParams } from "react-router-dom"
 
-const Movie  = async  () => {
+const Movie  =  () => {
     const [currentMovieDetail, setMovie] = useState()
     // має повернути ключ/значення, id з поточного URL-адреса ?
     const { id } = useParams()
@@ -20,7 +20,7 @@ const Movie  = async  () => {
         .then(data => setMovie(data))
     }
 
-    return defer (
+    return (
         <div className="movie">
             <div className="movie_intro">
                 <img className="movie_back" 
