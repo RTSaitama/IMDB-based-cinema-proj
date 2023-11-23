de1d80bdee567f565654cf5eee218be8f105beac
“RTSaitama” <RTSaitama@outlook.com>
2023-11-23 17:16:57 +0200

    carousel=>swiper changed and styles struct.rebdin

---
+++ src/components/logo/Logo.js
@@ -0,0 +1,38 @@
+export default   function Logo() {
+    return  (
+     
+ <div className="logo_wrapper header_logo"> 
+ 
+  <svg
+    className="logo"
+    width="800px"
+    height="800px"
+    viewBox="0 0 24 24"
+    fill="var(--logo-color, #030c1fb0;)"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      opacity=".6"
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M3 4h18a1 1 0 0 1 1 1v1H2V5a1 1 0 0 1 1-1z"
+      fill="var(--logo-color, #1356e4b0)"
+    />
+    <path
+      opacity=".3"
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M5 2h14a1 1 0 0 1 1 1v1H4V3a1 1 0 0 1 1-1z"
+      fill="var(--logo-color, #a5a4a4)"
+    />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M1 6h22a1 1 0 0 1 1 1v15a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1zm0 2v3h3V8H1zm0 5v3h3v-3H1zm0 5v3h3v-3H1zM20 8v3h3V8h-3zm0 5v3h3v-3h-3zm0 5v3h3v-3h-3z"
+      fill="var(--logo-color,#a5a4a4)"
+    />
+  </svg>
+  </div>
+
+    )
+}
